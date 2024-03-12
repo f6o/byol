ba244bcabcee5dd99205c94eae95552958291ea4
f6o <fluffy6h@gmail.com>
2024-03-12 17:02:36 +0900

    add input check

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ package main
 import "C"
 import (
 	"fmt"
+	"regexp"
 	"unsafe"
 
 	"github.com/f6o/byol/parser"
@@ -16,7 +17,15 @@ func main() {
 	cs := C.CString("lispy> ")
 	for {
 		input := C.readline(cs)
-		C.add_history(input)
+		line := C.GoString(input)
+		matched, err := regexp.MatchString(`[a-zA-Z0-9]+`, line)
+		if err != nil {
+			return
+		}
+		if matched {
+			fmt.Println("add history:", line)
+			C.add_history(input)
+		}
 		fmt.Printf("No you're a %s\n", C.GoString(input))
 		C.free(unsafe.Pointer(input))
 		state := parser.Mpc_state_new()
