aaa8107528539f3b1cf4b3538a540d146f6a2f25
f6o <fluffy6h@gmail.com>
2024-03-26 20:24:07 +0900

    print tags

--- mpc/example/lispy.c
+++ mpc/example/lispy.c
@@ -1,5 +1,13 @@
 #include "../mpc.h"
 
+void print_tag(mpc_ast_t* a, int depth) {
+    printf("[%d]%s\n", depth, a->tag);
+    for ( int i = 0; i < a->children_num; i++ ) {
+        mpc_ast_t* child = a->children[i];
+        print_tag(child, depth+1);
+    }
+}
+
 int main(int argc, char** argv) {
     mpc_parser_t* Number = mpc_new("number");
     mpc_parser_t* Operator = mpc_new("operator");
@@ -23,6 +31,10 @@ int main(int argc, char** argv) {
 
     if (mpc_parse("<stdin>", argv[1], Lispy, &r)) {
         mpc_ast_print(r.output);
+
+        printf("PRINT TAG ONLY\n");
+        print_tag(r.output, 0);
+
         mpc_ast_delete(r.output);
     } else {
         mpc_err_print(r.error);
