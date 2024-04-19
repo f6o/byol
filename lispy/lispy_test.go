b9b07fea93ddabf0bfdabf76b6d51c62adb4169a
f6o <fluffy6h@gmail.com>
2024-04-19 18:21:42 +0900

    fix

--- lispy/lispy_test.go
+++ lispy/lispy_test.go
@@ -4,38 +4,9 @@ import (
 	"testing"
 )
 
-func TestPrint(t *testing.T) {
+func ast01() (AST, int) {
 	// * 10 (+ 1 51)
-	root := AST{">", "", make([]AST, 0)}
-
-	regex := AST{"regex", "", make([]AST, 0)}
-	a := AST{"operator", "*", make([]AST, 0)}
-	b := AST{"number", "10", make([]AST, 0)}
-	c := AST{"expr", "", make([]AST, 0)}
-
-	h := AST{"char", "(", make([]AST, 0)}
-	i := AST{"operator", "+", make([]AST, 0)}
-	j := AST{"number", "1", make([]AST, 0)}
-	k := AST{"number", "51", make([]AST, 0)}
-	l := AST{"char", ")", make([]AST, 0)}
-
-	c.Children = append(c.Children, h)
-	c.Children = append(c.Children, i)
-	c.Children = append(c.Children, j)
-	c.Children = append(c.Children, k)
-	c.Children = append(c.Children, l)
-
-	root.Children = append(root.Children, regex)
-	root.Children = append(root.Children, a)
-	root.Children = append(root.Children, b)
-	root.Children = append(root.Children, c)
-	root.Children = append(root.Children, regex)
-
-	root.Print(0)
-}
-
-func TestEval(t *testing.T) {
-	// * 10 (+ 1 51)
+	ans := 520
 	root := AST{">", "", make([]AST, 0)}
 
 	regex := AST{"regex", "", make([]AST, 0)}
@@ -61,7 +32,11 @@ func TestEval(t *testing.T) {
 	root.Children = append(root.Children, c)
 	root.Children = append(root.Children, regex)
 
-	ans := 520
+	return root, ans
+}
+
+func TestEval(t *testing.T) {
+	root, ans := ast01()
 	if x, ok := root.Eval().(LVNumber); ok && x.number != ans {
 		t.Errorf("Eval %d; expected %d", x, ans)
 	}
